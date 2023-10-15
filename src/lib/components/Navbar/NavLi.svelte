5962ff0fc70a88f5fd92bc410dc40477d3bbd323
TMK04 <floer.learner.01@gmail.com>
2023-10-15 20:12:16 +0800

    fix(Navbar): keep search icon dark

---
+++ src/lib/components/Navbar/NavLi.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { NavLi } from 'flowbite-svelte';
+
+	export let href: string;
+	export let active = false;
+</script>
+
+<NavLi {href} {active}>
+	<span
+		class="relative font-bold after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-3/4 after:origin-left after:scale-0 after:bg-white/0 after:transition-all after:content-[''] after:hocus:scale-100 after:hocus:bg-white/100"
+	>
+		<slot />
+	</span>
+</NavLi>
--- src/lib/components/Navbar/index.svelte
+++ src/lib/components/Navbar/index.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import { TITLE } from '$lib/config';
-	import { Button, Input, NavBrand, NavHamburger, NavLi, NavUl, Navbar } from 'flowbite-svelte';
+	import { Button, Input, NavBrand, NavHamburger, NavUl, Navbar } from 'flowbite-svelte';
 	import { SearchOutline } from 'flowbite-svelte-icons';
+	import NavLi from './NavLi.svelte';
 
 	export let bg: boolean;
 </script>
 
 <div class="relative">
-	<Navbar class="fixed transition-colors {bg ? '' : 'bg-transparent text-white'}">
+	<Navbar class="fixed transition-colors {bg ? '' : 'bg-transparent text-white'} py-1">
 		<NavBrand href="/">
 			<img src="/favicon.png" class="mr-3 h-6 sm:h-9" alt={TITLE} />
 			<span class="self-center whitespace-nowrap text-xl font-bold">
@@ -15,7 +16,7 @@
 			</span>
 		</NavBrand>
 		<!-- TODO: Restyle Search -->
-		<div class="flex text-gray-700">
+		<div class="flex">
 			<Button
 				color="none"
 				data-collapse-toggle="mobile-menu-3"
@@ -26,28 +27,20 @@
 				<SearchOutline class="h-5 w-5" />
 			</Button>
 			<div class="relative hidden md:block">
-				<div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+				<div
+					class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 text-gray-700"
+				>
 					<SearchOutline class="h-4 w-4" />
 				</div>
 				<Input id="search-navbar" class="pl-10" placeholder="Search..." />
 			</div>
 			<NavHamburger />
 		</div>
-		<NavUl
-			nonActiveClass="navbar-navli relative after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-3/4 after:origin-left after:scale-0 after:bg-white/0 after:transition-all after:content-[''] after:hocus:scale-100 after:hocus:bg-white/100"
-		>
-			<NavLi href="/" active={true}>
-				<span class="font-bold">Home</span>
-			</NavLi>
-			<NavLi href="/properties">
-				<span class="font-bold">Properties</span>
-			</NavLi>
-			<NavLi href="/contact">
-				<span class="font-bold">Contact</span>
-			</NavLi>
-			<NavLi href="/news">
-				<span class="font-bold">News</span>
-			</NavLi>
+		<NavUl nonActiveClass="">
+			<NavLi href="/" active={true}>Home</NavLi>
+			<NavLi href="/properties">Properties</NavLi>
+			<NavLi href="/contact">Contact</NavLi>
+			<NavLi href="/news">News</NavLi>
 		</NavUl>
 	</Navbar>
 </div>
