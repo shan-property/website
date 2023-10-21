47953131ff4a52b0bb03148d4ed16279abdaba38
TMK04 <floer.learner.01@gmail.com>
2023-10-21 21:29:50 +0800

    refactor(search colors): make responsive

--- src/lib/components/Navbar/index.svelte
+++ src/lib/components/Navbar/index.svelte
@@ -21,20 +21,19 @@
 				{TITLE}
 			</span>
 		</NavBrand>
-		<!-- TODO: Restyle Search -->
 		<div class="flex">
 			<Button
 				color="none"
 				data-collapse-toggle="mobile-menu-3"
 				aria-controls="mobile-menu-3"
 				aria-expanded="false"
-				class="mr-1 rounded-lg p-2.5 text-sm text-secondary hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700 md:hidden"
+				class="mr-1 rounded-lg p-2.5 text-sm md:hidden"
 			>
 				<SearchOutline class="h-5 w-5" />
 			</Button>
 			<div class="relative hidden md:block">
 				<div
-					class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 text-gray-700"
+					class="text-secondary-variant pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
 				>
 					<SearchOutline class="h-4 w-4" />
 				</div>
