7ad3525f1c3c528679267d0d2a5af980dec00611
TMK04 <floer.learner.01@gmail.com>
2023-10-22 23:37:23 +0800

    refactor(Navbar): make file for Search, match Search & DarkMode colors

---
+++ src/lib/components/Navbar/Search.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Button, Input } from 'flowbite-svelte';
+	import { SearchOutline } from 'flowbite-svelte-icons';
+</script>
+
+<Button
+	color="none"
+	data-collapse-toggle="mobile-menu-3"
+	aria-controls="mobile-menu-3"
+	aria-expanded="false"
+	class="text-secondary-light mr-1 rounded-lg p-2.5 text-sm hocus:text-text md:hidden"
+>
+	<SearchOutline class="h-5 w-5" />
+</Button>
+<div class="relative hidden md:block">
+	<div
+		class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 text-secondary-variant"
+	>
+		<SearchOutline class="h-4 w-4" />
+	</div>
+	<Input id="search-navbar" class="pl-10" placeholder="Search..." />
+</div>
--- src/lib/components/Navbar/index.svelte
+++ src/lib/components/Navbar/index.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import { TITLE } from '$lib/config';
-	import { Button, Input, NavBrand, NavHamburger, NavUl } from 'flowbite-svelte';
-	import { SearchOutline } from 'flowbite-svelte-icons';
+	import { Button, NavBrand, NavHamburger, NavUl } from 'flowbite-svelte';
 	import DarkMode from '../DarkMode.svelte';
 	import Favicon from '../Svgs/Favicon.svelte';
 	import NavLi from './NavLi.svelte';
+	import Search from './Search.svelte';
 
 	export let bg: boolean;
 </script>
@@ -28,24 +28,8 @@
 					{TITLE}
 				</span>
 			</NavBrand>
-			<div class="flex">
-				<Button
-					color="none"
-					data-collapse-toggle="mobile-menu-3"
-					aria-controls="mobile-menu-3"
-					aria-expanded="false"
-					class="mr-1 rounded-lg p-2.5 text-sm md:hidden"
-				>
-					<SearchOutline class="h-5 w-5" />
-				</Button>
-				<div class="relative hidden md:block">
-					<div
-						class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 text-secondary-variant"
-					>
-						<SearchOutline class="h-4 w-4" />
-					</div>
-					<Input id="search-navbar" class="pl-10" placeholder="Search..." />
-				</div>
+			<div class="flex text-text">
+				<Search />
 				<DarkMode btnClass="block md:hidden" />
 				<NavHamburger />
 			</div>
