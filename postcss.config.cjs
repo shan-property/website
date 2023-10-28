87f25b139f2c0ada8f66d9de5ab847b02fd8f6da
TMK04 <floer.learner.01@gmail.com>
2023-10-28 18:26:04 +0800

    fix(css nesting): add tailwindcss/nesting in postcss config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,11 @@
+const tailwindcss_nesting = require('tailwindcss/nesting');
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 
 const config = {
 	plugins: [
+		// tailwindcss_nesting,
+		tailwindcss_nesting(),
 		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
 		tailwindcss(),
 		//But others, like autoprefixer, need to run after,
