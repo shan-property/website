995d2dddeb7c03c1bd79fc2041c7b48eb99916d9
TMK04 <floer.learner.01@gmail.com>
2023-10-28 17:06:15 +0800

    feat: add Featured section (headers)

---
+++ src/lib/components/Featured.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let title: string;
+</script>
+
+<section class="px-[10%]">
+	<h2 class="afterline afterline-base inline font-serif text-xl after:bg-accent">{title}</h2>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Featured from '$lib/components/Featured.svelte';
 	import Hero from '$lib/components/Hero.svelte';
 	import { TITLE } from '$lib/config';
 </script>
@@ -6,3 +7,9 @@
 <Hero />
 
 <h1 class="text-center text-primary-variant">{TITLE}</h1>
+
+<article class="flex flex-col gap-9">
+	<Featured title="Featured Apartments for Rent" />
+	<Featured title="Featured Apartments for Sale" />
+	<Featured title="Auction" />
+</article>
