ec18e2e5f657eb70fa7a3cbd589fa87cd0ebbe3a
TMK04 <floer.learner.01@gmail.com>
2023-11-13 16:09:03 +0800

    refactor(PropertyCard Tag): enlarge, hide expandable on <sm screens

--- src/lib/components/PropertyCard/Tag.svelte
+++ src/lib/components/PropertyCard/Tag.svelte
@@ -5,6 +5,8 @@
 
 	export let tag: string;
 
+	const Icon_class = 'h-6 w-5 sm:h-5 sm:w-4';
+
 	$: Icon_promise = import(`$lib/components/Svgs/${tag}.svelte`) as Promise<{
 		default: typeof SvelteComponent & { props: SvgProps };
 	}>;
@@ -14,12 +16,12 @@
 	class="relative mt-0.25 flex rounded-bl-sm rounded-tr-sm bg-accent py-0.25 pe-1.25 ps-1 text-end text-sm sm:text-start"
 >
 	{#await Icon_promise}
-		<Loading class="h-5 w-4" />
+		<Loading class={Icon_class} />
 	{:then { default: Icon }}
-		<svelte:component this={Icon} class="h-5 w-4" />
+		<svelte:component this={Icon} class={Icon_class} />
 	{/await}
 	<div
-		class="rest-letters absolute left-full top-0 z-10 -ms-0.5 overflow-hidden rounded-bl-sm rounded-tr-sm bg-inherit py-0.25 transition-all"
+		class="rest-letters absolute left-full top-0 z-10 -ms-0.5 hidden overflow-hidden rounded-bl-sm rounded-tr-sm bg-inherit py-0.25 transition-all sm:block"
 	>
 		{tag}
 	</div>
