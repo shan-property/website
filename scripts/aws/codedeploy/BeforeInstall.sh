0742796221fbfcd738335e0223e371b128817f6c
TMK04 <floer.learner.01@gmail.com>
2023-11-27 11:40:45 +0800

    ci(.env): link .env to build
    svelte static envs are loaded during build, not runtime
    Which also means we don't need dotenv

--- scripts/aws/codedeploy/BeforeInstall.sh
+++ scripts/aws/codedeploy/BeforeInstall.sh
@@ -2,6 +2,7 @@
 
 # Change directory to the website root
 WEBSITE_CODEDEPLOY_TMP="/opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive"
+WEBSITE_CODEDEPLOY_DIR="/home/ec2-user/website"
 cd $WEBSITE_CODEDEPLOY_TMP
 
 # Debug
@@ -10,6 +11,9 @@ sudo ls -la
 
 # Install dependencies
 sudo pnpm install --prod
+
+# Copy the environment file
+sudo ln -s "$WEBSITE_CODEDEPLOY_DIR/.env" .env
 
 # Build the application
 sudo pnpm run build
