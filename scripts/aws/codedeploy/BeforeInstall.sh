c8c05babf1ea73e7b28cdc99dc2f1904794ff935
TMK04 <floer.learner.01@gmail.com>
2023-11-27 11:24:07 +0800

    fix(BeforeInstall): cd to deployment tmp

--- scripts/aws/codedeploy/BeforeInstall.sh
+++ scripts/aws/codedeploy/BeforeInstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Change directory to the website root
+WEBSITE_CODEDEPLOY_TMP="/opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive"
+cd $WEBSITE_CODEDEPLOY_TMP
+
 # Debug
 sudo pwd
 sudo ls -la
